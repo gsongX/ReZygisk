c6d2f3c10c6bf5cab3702a3d5f4cbc0ed6f7a753
MONA <121730102+MeowDump@users.noreply.github.com>
2026-09-02 09:51:15 +0530

    Add check for ReZygiskd before hooking functions
    Signed-off-by: MONA <121730102+MeowDump@users.noreply.github.com>

--- loader/src/injector/entry.c
+++ loader/src/injector/entry.c
@@ -12,20 +12,22 @@ void entry(void *addr, size_t size, int tango_flag) {
   start_addr = addr;
   block_size = size;
 
+  if (!rezygiskd_zygote_injected()) {
+    LOGE("ReZygiskd is not running");
+    return;
+  }
+
   LOGD("start plt hooking");
-  hook_functions();
+  if (!hook_functions()) {
+    LOGE("Failed to initialize PLT hooks; leaving zygote unmodified");
+    return;
+  }
 
   struct kernel_version version = parse_kversion();
   if (version.major > 3 || (version.major == 3 && version.minor >= 8)) {
     LOGD("Supported kernel version %d.%d.%d, sending seccomp event", version.major, version.minor, version.patch);
 
     perform_ptrace_message_clear();
-  }
-
-  if (!rezygiskd_zygote_injected()) {
-    LOGE("ReZygiskd is not running");
-
-    return;
   }
 
   LOGD("Zygisk library execution done, addr: %p, size: %zu", addr, size);
